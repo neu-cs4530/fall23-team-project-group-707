315396f3738491f7516594bf9c5d8f6631e7b522
mscMstr <delgadocarla2021@gmail.com>
2023-11-02 01:46:13 -0400

    wrote a preliminary test plan for SuggestionForm in frontend

---
+++ frontend/src/components/Town/interactables/SuggestionForm.test.tsx
@@ -0,0 +1,18 @@
+describe('Suggestion form', () => {
+  describe('Test SuggestionForm', () => {
+    it('renders correct form', () => {
+      // write test here
+    });
+    test('search button', () => {
+      // write test here
+    });
+    test('add button', () => {
+      // write test here
+    });
+  });
+  describe('Test SuggestionFormWrapper', () => {
+    it('renders correct suggestion form', () => {
+      // write test here
+    });
+  });
+});
