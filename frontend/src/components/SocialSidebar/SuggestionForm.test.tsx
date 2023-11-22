f76ea9409059e36f80fa3a75f402a1359380e686
Dat Ngo <datngonyc@gmail.com>
2023-11-22 18:47:32 -0500

    deleted old unused test file

--- frontend/src/components/SocialSidebar/SuggestionForm.test.tsx
+++ frontend/src/components/SocialSidebar/SuggestionForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { SuggestionForm } from './SuggestionForm';
 import JukeboxAreaController from '../../classes/interactable/JukeboxAreaController';
 import { nanoid } from 'nanoid';
